db1be6874414abf444a8f56d02323295da0a0486
gfu <gfu@marchex.com>
2015-06-16 16:30:16 -0700

    Incremental work    - StreamPlayer

--- src/main/java/com/gfu/app/StreamPlayer.java
+++ src/main/java/com/gfu/app/StreamPlayer.java
@@ -1,4 +1,55 @@
 package com.gfu.app;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import java.io.IOException;
+
 public class StreamPlayer {
+    AudioFile audioFile;
+
+    public StreamPlayer(AudioFile audioFile) {
+        this.audioFile = audioFile;
+    }
+
+    public void run() {
+        try {
+            play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() throws LineUnavailableException, IOException {
+        AudioInputStream din = audioFile.decodedInputStream;
+        byte[] data = new byte[4096];
+        SourceDataLine line = getLine(audioFile.getDecodedFormat());
+
+        if (line != null) {
+            line.start();
+
+            int nBytesRead = 0;
+            while (nBytesRead != -1) {
+                nBytesRead = din.read(data, 0, data.length);
+                if (nBytesRead != -1) {
+                    line.write(data, 0, nBytesRead);
+                }
+            }
+
+            line.drain();
+            line.stop();
+            line.close();
+        }
+    }
+
+    private SourceDataLine getLine(AudioFormat audioFormat) throws LineUnavailableException {
+        SourceDataLine res;
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+        res = (SourceDataLine) AudioSystem.getLine(info);
+        res.open(audioFormat);
+        return res;
+    }
 }
