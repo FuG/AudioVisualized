fb8aa0bf02fd3d4257f4eba6e7feb191b5cb80c6
gfu <gfu@marchex.com>
2015-06-17 16:03:03 -0700

    Incremental work    - DSP

---
+++ src/main/java/com/gfu/app/visual/DSP.java
@@ -0,0 +1,25 @@
+package com.gfu.app.visual;
+
+import com.gfu.app.AudioFile;
+
+import javax.sound.sampled.AudioInputStream;
+import java.util.List;
+
+public class DSP {
+
+    public static List<double[]> generateFrequencySpectrums(AudioFile audioFile) {
+        return null;
+    }
+
+    public static List<double[]> generateAmplitudeSpectrums(AudioFile audioFile) {
+        return null;
+    }
+
+    private double[] decodeToDoubles(AudioInputStream inputStream) {
+        return null;
+    }
+
+    private double[] transform(double[] input) {
+        return null;
+    }
+}
