540bd1dad8fe28726ea7f9f79b8b6b701919991a
gfu <fu.gary.2009@gmail.com>
2015-06-18 22:41:09 -0700

    Incremental Changes:   - AudioFile reads inputStream into byteBuffer

--- src/main/java/com/gfu/app/AudioFile.java
+++ src/main/java/com/gfu/app/AudioFile.java
@@ -4,6 +4,7 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -15,11 +16,33 @@ public class AudioFile {
 
     private AudioFormat baseFormat;
 
+    private byte[] byteBuffer;
+
+    private double[] doubleBuffer;
+
     public AudioFile(String filepath) throws IOException, UnsupportedAudioFileException, URISyntaxException {
         ClassLoader classLoader = getClass().getClassLoader();
         file = new File(classLoader.getResource(filepath).getFile());
 
         init();
+
+        loadByteBuffer();
+    }
+
+    private void loadByteBuffer() throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] data = new byte[4096];
+
+        int nBytesRead = 0;
+        while(nBytesRead != -1) {
+            nBytesRead = baseInputStream.read(data, 0, data.length);
+            if (nBytesRead != -1) {
+                out.write(data, 0, nBytesRead);
+            }
+
+        }
+
+        byteBuffer = out.toByteArray();
     }
 
     private void init() throws IOException, UnsupportedAudioFileException {
